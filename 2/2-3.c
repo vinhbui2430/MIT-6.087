88ad05fb4fd4269cef386d12a3a3419c528a2d38
vinh_onfedora <cvinh312@gmail.com>
2025-01-07 11:46:10 +0700

    Complete solution code for problem 2.3

--- 2/2-3.c
+++ 2/2-3.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
-int val =0xCAFE; 
+int val =0xCAFE, new_val; 
 int CHECK_ON_BITS();
+int REVERSE_BYTE_ORDER();
+int ROTATE_FOUR_BITS();
 int main()
 {
     int CHECK_ON_BITS(int val, int nbit)
@@ -12,5 +14,17 @@ int main()
         else return printf("not all three of last 4 bits are on \n");
     }
     // printf("%d \n", (val & 0x7));
-    return CHECK_ON_BITS(val, 0x7);
+    int REVERSE_BYTE_ORDER(int val)
+    {
+        return val = ((val & ~0xFF00)<<8 | (val>>0x8));
+    }    
+    new_val = REVERSE_BYTE_ORDER(val);
+    printf("byte order reversed: %X \n", new_val); 
+    int ROTATE_FOUR_BITS(int val)
+    {
+        int ls4b = val & ~0xFFF0;  
+        return val = (ls4b<<12) | ((val & ~0xF)>>4);     
+    }
+    printf("rotated fourbits: %X \n", ROTATE_FOUR_BITS(val)); 
+    return 0;
 }
