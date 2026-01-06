ed1878b65029b02a1fd5799eee6972df7fdbdcc8
vinh_onfedora <cvinh312@gmail.com>
2026-01-06 17:29:53 +0700

    update assn #2 solution

--- 2/2-1.c
+++ 2/2-1.c
@@ -2,13 +2,13 @@
 #include<limits.h>
 #include<float.h>
 
-void main(){
-    printf("size,minimum and maximum value of char data type: %d bytes, %d, %d \n", sizeof(char), SCHAR_MIN, SCHAR_MAX);
-    printf("size,minimum and maximum value of of unsigned char data type: %d bytes, %d, %d \n", sizeof(unsigned char), CHAR_MIN, CHAR_MAX);
-    printf("size,minimum and maximum value of of short data type: %d bytes, %d, %d \n", sizeof(short), SHRT_MIN, SHRT_MAX);
-    printf("size,minimum and maximum value of of int data type: %d bytes, %d, %d \n", sizeof(int), INT_MIN, INT_MAX);
-    printf("size,minimum and maximum value of of unsigned int data type: %d bytes, %d, %ld \n", sizeof(unsigned int), 0 , UINT_MAX);
-    printf("size,minimum and maximum value of of long data type: %d bytes, %ld, %ld \n", sizeof(long), LONG_MIN, LONG_MAX);
-    printf("size,minimum and maximum value of of float data type: %d bytes, %.10e, %.10e \n", sizeof(float), FLT_MIN, FLT_MAX);
-    return;
+int main(){
+    printf("My machine is 64 bits");
+    printf("char data type: size=%d, minimum value=%d, maximum value=%d \n", sizeof(char), CHAR_MIN, CHAR_MAX);
+    printf("unsigned char data type: size=%d, minimum value=%d, maximum value=%d \n", sizeof(unsigned char), 0, UCHAR_MAX);
+    printf("int data type: size=%d, minimum value=%d, maximum value=%d \n", sizeof(int), INT_MIN, INT_MAX);
+    printf("unsigned int data type: size=%d, minimum value=%d, maximum value=%u \n", sizeof(unsigned int), 0, UINT_MAX);
+    printf("unsigned long data type: size=%d, minimum value=%d, maximum value=%u \n", sizeof(unsigned long), 0, ULONG_MAX);
+    printf("float data type: size=%d, minimum value=%f, maximum value=%f \n", sizeof(float), FLT_MIN, FLT_MAX);
+    return 0; 
 }
--- 3/3-1.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("This is the solution for 3-1");
-    return 0;
-}
